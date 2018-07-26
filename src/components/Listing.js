648c00e88c94a6efec71bbcb1f730eb270ab4638
Alexandra Ralev <alexandrapredaralev@gmail.com>
2018-07-26 13:02:32 +0100

    show street view

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -6,7 +6,7 @@ class Listing extends Component {
     render () {
         
         return (
-            <div className="listing" onMouseEnter={(e) => this.props.flag(e.target)}>
+            <div className="listing" onClick={(e) => this.props.flag(e.target)}>
                 <h3>{this.props.location.name}</h3>
                 <img src={this.props.location.categories[0].icon.prefix+'bg_'+32+this.props.location.categories[0].icon.suffix} alt={this.props.location.name}/>
                 <p className="category">{this.props.location.categories[0].name}</p>
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -36,7 +36,8 @@ class MapContainer extends Component {
     const google = this.props.google
     let infowindow = this.state.infowindow
     let places = []
-    let markers =[]
+    let markers = []
+
     //only display markers for filtered location if there's a filtered locations list
     if (this.props.filteredPlaces[0]) {
       places = this.props.filteredPlaces
@@ -54,34 +55,65 @@ class MapContainer extends Component {
         title: place.name
       })
 
+      markers.push(marker)
+
       marker.addListener('click', () => {
         this.populateInfoWindow(marker, infowindow)
       })
 
-      markers.push(marker)
-     bounds.extend(marker.position)
-    })
-    this.map.fitBounds(bounds)
-    this.setState((state) => ({
-        markers : markers
-      }))
-  }
+      bounds.extend(marker.position)
 
+    })
+
+    this.map.fitBounds(bounds)
+
+    this.setState({markers})
+  }
   populateInfoWindow = (marker, infowindow) => {
-    const defaultIcon = marker.getIcon()
     const {highlightedIcon, markers} = this.state
+    const google = this.props.google
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker !== marker) {
+
       infowindow.marker = marker
-      infowindow.setContent(`<h3>${marker.title}</h3>`)
-      infowindow.open(this.map, marker)
+      
       // Make sure the marker property is cleared if the infowindow is closed.
       infowindow.addListener('closeclick', function () {
         infowindow.marker = null
       })
+
+      infowindow.setContent('<div>' + marker.title + '</div><div id="pano">Street View Loading</div>')
+      let panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'))
+
+      //code for showing location in infowindow
+
+      let sw = new google.maps.StreetViewService()
+
+      sw.getPanorama({location: marker.position, radius: 50}, (results, status) => {
+        if (status === 'OK') {
+
+          let heading = google.maps.geometry.spherical.computeHeading(results.location.latLng, marker.position)
+          panorama.setPano(results.location.pano);
+          panorama.setPov({
+              heading: heading,
+              pitch: 0
+            });
+          panorama.setVisible(true)
+          
+
+        } else {
+          infowindow.setContent('<div>' + marker.title + '</div>' +
+            '<div>No Street View Found</div>')
+        }
+      })
+
+      
+
+      // Open the infowindow on the correct marker.
+      infowindow.open(this.map, marker)
     }
   }
-
+  
 
   render() {
 
